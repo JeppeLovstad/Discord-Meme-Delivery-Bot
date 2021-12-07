3c845b05b1132d8a226f8abc2c8fd1d92625081a
jeppe <jeppemlv@gmail.com>
2021-12-07 18:12:45 +0100

    updated scraping logic

--- GetMeme.py
+++ GetMeme.py
@@ -1,27 +1,45 @@
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import time
+import random
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class MemeScraper:
-    meme_URL = 'https://imgflip.com/ai-meme'
-    meme_index = {}
-    ImageLink = None
-    driver = None
+    meme_URL: str = 'https://imgflip.com/ai-meme'
+    meme_index_dict: dict = {}
+    driver: driver = None
 
-    def updateImage(self):
+    def updateImage(self, meme_index: int = None, ) -> str:
         driver = self.driver
         driver.get(self.meme_URL)
+        meme_number = len(self.meme_index_dict)-1
 
-        time.sleep(3)
+        # Get random meme, if not specified
+        if not meme_index or meme_index > meme_number or meme_index < 0:
+            meme_index = random.randint(0, meme_number)
+
+        self.checkLoading(By.ID, "site-loading")
+
+        driver.find_element(
+            By.CSS_SELECTOR, f"div[data-index='{meme_index}']").click()
+
+        self.checkLoading(By.ID, "site-loading")
 
         driver.find_element(By.CLASS_NAME, 'aim-generate-btn').click()
-        time.sleep(1)
-        self.ImageLink = driver.find_element(By.CLASS_NAME,
-                                             'img-code').get_attribute("value")
+
+        self.checkLoading(By.CLASS_NAME, "img-code", True)
+
+        return driver.find_element(By.CLASS_NAME,
+                                   'img-code').get_attribute("value")
+
+    def getAvailableMemes(self) -> dict:
+        return self.meme_index_dict
 
     def initiateDriver(self):
         options = Options()
@@ -29,20 +47,40 @@ class MemeScraper:
         driver = webdriver.Firefox(
             options=options,
             executable_path='geckodriver.exe')
-        driver.implicitly_wait(30)
+        driver.implicitly_wait(0)
         driver.set_window_size(1920, 1080)
-        driver.get(self.meme_URL)
+        # driver.get(self.meme_URL)
         self.driver = driver
+
+    def checkLoading(self, by: By, locator, stop_when_found: bool = False):
+        driver = self.driver
+        try:
+            # wait for loading element to appear
+            # - required to prevent prematurely checking if element
+            #   has disappeared, before it has had a chance to appear
+            WebDriverWait(driver, 5
+                          ).until(EC.presence_of_element_located((by, locator)))
+
+            # then wait for the element to disappear
+            if not stop_when_found:
+                WebDriverWait(driver, 30
+                              ).until_not(EC.presence_of_element_located((by, locator)))
+
+        except TimeoutException:
+            # if timeout exception was raised - it may be safe to
+            # assume loading has finished, however this may not
+            # always be the case, use with caution, otherwise handle
+            # appropriately.
+            pass
 
     def update_available_memes(self):
         req = requests.get(self.meme_URL)
-        # print(req.text)
         soup = BeautifulSoup(req.text, "html.parser")
         t = soup.find_all(class_="aim-meme-btn")
         for button in t:
             memeName = button.find("img").get("title")
             memeIndex = button.get("data-index")
-            self.meme_index[memeIndex] = memeName
+            self.meme_index_dict[memeIndex] = memeName
 
     def __init__(self) -> None:
         self.update_available_memes()
@@ -53,5 +91,7 @@ class MemeScraper:
 
 
 # m = MemeScraper()
-# m.updateImage()
-# print(m.ImageLink)
+# # m.updateImage()
+# messages = '\n'.join([f"{k} : {v}" for k,
+#                       v in m.meme_index_dict.items()])
+# print(messages)
--- Memes.py
+++ Memes.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import base64
 
 client = discord.Client()
-m = GetMeme.MemeScraper()
+scraper = GetMeme.MemeScraper()
 
 
 @client.event
@@ -17,13 +17,23 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    initial_command = message.content.split()[0]
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-    if message.content.startswith('$boi'):
-        m.updateImage()
-        #file = discord.File(BytesIO(base64.b64decode(m.Image)))
-        await message.channel.send(m.ImageLink)
+    if initial_command == '$memelist':
+        message_to_send = 'index: Meme name\n'
+        message_to_send += '\n'.join([f"{k} : {v}" for k,
+                                      v in scraper.meme_index_dict.items()])
+        await message.channel.send(message_to_send)
+    if initial_command == '$meme':
+        incoming_message = message.content.split()
+        send = ""
+        if len(incoming_message) > 1 and incoming_message[1].isdigit():
+            send = scraper.updateImage(int(incoming_message[1]))
+        else:
+            send = scraper.updateImage()
+        await message.channel.send(send)
     if "69" in message.content:
         await message.channel.send('lmao')
     if "96" in message.content:
@@ -32,4 +42,4 @@ async def on_message(message):
         await message.channel.send('get rekt kiddo')
 
 # print(os.getenv('DISCORD_TOKEN'))
-client.run({REDACTED})
+client.run({REDACTED}})
