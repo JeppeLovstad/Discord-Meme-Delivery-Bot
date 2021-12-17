200b1740f676ccceecea7a725fb9e5cf81600e75
ChristofferCJ <christofferchangtrakuljensen@gmail.com>
2021-12-17 15:39:06 +0100

    pepe classes, rejoice!

--- Pepe.py
+++ Pepe.py
@@ -2,13 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 from random import choice
 
-pepe_url = 'https://rare-pepe.com/'
+class Pepe:
+    def __init__(self):
+        pepe_url = 'https://rare-pepe.com/'
 
-pepe_html = requests.get(pepe_url).content
+        pepe_html = requests.get(pepe_url).content
 
-soup = BeautifulSoup(pepe_html, 'html.parser')
-img_tag = soup.find_all('img', class_='attachment-thumbnail size-thumbnail')
-pepe_imgs = [img['src'] for img in img_tag]
-
-def get_pepe():
-    return choice(pepe_imgs)
+        soup = BeautifulSoup(pepe_html, 'html.parser')
+        img_tag = soup.find_all('img', class_='attachment-thumbnail size-thumbnail')
+        self.pepe_imgs = [img['src'] for img in img_tag]
+    
+    from Memes import bot
+    @bot.command()
+    def pepe(self):
+        return choice(self.pepe_imgs)
