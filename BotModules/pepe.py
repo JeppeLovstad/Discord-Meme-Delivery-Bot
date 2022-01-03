bd02dd848ce253286d3be1eed78d3670df353b1a
Jeppe Løvstad <jml@solitwork.dk>
2022-01-03 12:57:06 +0100

    pepe test

--- BotModules/pepe.py
+++ BotModules/pepe.py
@@ -3,21 +3,32 @@ import requests
 from random import choice
 from discord.ext import commands
 
+
 class Pepe(commands.Cog):
-    def __init__(self,config, bot: commands.Bot):
+    def __init__(self, config, bot: commands.Bot):
         self.bot = bot
         self.config = config
-        
-        pepe_url = 'https://rare-pepe.com/'
+
+        pepe_url = "https://rare-pepe.com/"
         pepe_html = requests.get(pepe_url).content
-        soup = BeautifulSoup(pepe_html, 'html.parser')
-        img_tag = soup.find_all('img', class_='attachment-thumbnail size-thumbnail')
-        self.pepe_imgs = [img['src'] for img in img_tag]
-    
+        soup = BeautifulSoup(pepe_html, "html.parser")
+        img_tag = soup.find_all("img", class_="attachment-thumbnail size-thumbnail")
+        self.pepe_imgs = [img["src"] for img in img_tag]
+
     @commands.command()
     async def pepe(self, ctx):
         pepe_img = self.get_pepe()
         await ctx.send(pepe_img)
-    
+
     def get_pepe(self):
         return choice(self.pepe_imgs)
+
+
+if __name__ == "__main__":
+    from configparser import ConfigParser
+
+    config = ConfigParser()
+    config.read("config.ini")
+    bot = commands.Bot(command_prefix="!")
+    m = Pepe(bot=bot, config=config["PEPE"])
+    print(m.get_pepe())
