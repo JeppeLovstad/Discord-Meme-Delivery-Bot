b58ae286dc8442de869b6454f10e8b6663c137a7
lasse morgen <lasse.m.morgen@gmail.com>
2022-01-03 22:54:56 +0100

    RedditScraper v1

--- GenericScrapers/RedditScrape.py
+++ GenericScrapers/RedditScrape.py
@@ -1,15 +1,26 @@
 import requests
 
+from random import choice
+
 class RedditScrape:
 
-    def __init__(self, sub_reddit, load_amount, refresh_time_in_hours):
+    post_dictionary = dict()
+    def __init__(self, sub_reddit = "python", load_amount = 25):
         self.sub_reddit = sub_reddit
         self.load_amount = load_amount
-        self.refresh_time_in_hours = refresh_time_in_hours
-       
+    
     #MemeBotButItsTrash
     #JeppeChrisLasse123
-    def get_reddit():
+    async def get_random_post(self):
+        if(len(self.post_dictionary) < 5 and len(self.post_dictionary > 0)):
+            self.populate_list()
+        if(len(self.post_dictionary) == 0):
+           await self.populate_list()
+        post =  choice(self.populate_list)
+        self.populate_list.pop(post)
+        return post
+
+    async def populate_list(self):
 
         # note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
         auth = requests.auth.HTTPBasicAuth('n_gaqCyYChHH3k1i6Ea2Tw', 'qh4_ESsCaGLSwYlK_n-omSjuiat9iw')
@@ -24,7 +35,7 @@ class RedditScrape:
 
         # send our request for an OAuth token
         res = requests.post('https://www.reddit.com/api/v1/access_token',
-                            auth=auth, data=data, headers=headers)
+                            auth=auth, data=data, headers=headers, limit=self.load_amount)
         
         # convert response to JSON and pull access_token value
         TOKEN = res.json()['access_token']
@@ -35,11 +46,11 @@ class RedditScrape:
         # while the token is valid (~2 hours) we just add headers=headers to our requests
         requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
 
-        res = requests.get("https://oauth.reddit.com/r/python/hot",
+        res = requests.get("https://oauth.reddit.com/r/"+self.sub_reddit+"/hot",
                    headers=headers)
 
+        #print(res.json())
         for post in res.json()['data']['children']:
-            print(post['data']['title'])
+            url = post['data']['url']
+            self.post_dictionary[url] = url
         
-r = RedditScrape
-r.get_reddit()
