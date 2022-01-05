f631db3639a9d6a6ee86eab65ee8e0658d9221ad
jeppe <jeppemlv@gmail.com>
2022-01-05 10:47:02 +0100

    Reordering

--- MemeBot.py
+++ MemeBot.py
@@ -9,12 +9,18 @@ class MemeBot:
     command_prefix: str
 
     def __init__(self, config: dict, bot: commands.Bot):
-        self.initiateBotModules(config, bot)
-        self.registerDefaultFunctions(bot)
         self.bot = bot
+        enabledCogs = self.getCogInfoFromConfig(config)
+        for cogInfo in enabledCogs:
+            cog = self.instantiateCog(cogInfo)
+            if cog:
+                bot.add_cog(cog)
+                
+        self.registerDefaultFunctions(bot)
         bot.run(config["DISCORD"]["bot_token"])
 
-    def initiateBotModules(self, config, bot):
+    def getCogInfoFromConfig(self, config):
+        cogs = []
         for module, enabled in config["BOT_MODULES"].items():
             if enabled == "True":
 
@@ -28,29 +34,40 @@ class MemeBot:
                     if f"{module}_path" in config["BOT_MODULES"]
                     else f"BotModules.{module}"
                 )
-                try:
-                    #get class from folder
-                    imported_class = self.import_bot_module(module_path, main_classname)
-                    #check if module specific config exists
-                    module_config = (
+                module_config = (
                         config[module.upper()] if module.upper() in config else {}
                     )
-                    #iniate class
-                    try:
-                        initiated_class = imported_class(bot=bot, config=module_config)
-                        initiated_class.__cog_name__ = module.capitalize()
-                    except Error as e:
-                        print(f"Could not instantiate {module}, {e.message}")
-                        continue
-                    #update class name
-                except ModuleNotFoundError:
-                    print(f"no module found in {module_path}")
-                    continue
-                except AttributeError:
-                    print(f"no class '{main_classname}', found in module {module_path}")
-                    continue
+                cog={"module":module, "main_classname":main_classname, "module_path":module_path, "module_config":module_config}
+                cogs.append(cog)
+        return cogs
 
-                bot.add_cog(initiated_class)
+    def instantiateCog(self, cog):
+        error_message = initiated_class = None
+        module,main_classname,module_path,module_config = cog["module"],cog["main_classname"],cog["module_path"],cog["module_config"]
+        try:
+            #get class from folder
+            imported_class = self.import_bot_module(module_path, main_classname)
+            #check if module specific config exists
+
+            try:
+                #initate class
+                initiated_class = imported_class(bot=self.bot, config=module_config)
+                #update class name
+                initiated_class.__cog_name__ = module.capitalize()
+            except Error as e:
+                print(f"Could not instantiate {module} skipping..., \n Error: {e.message}")
+                error_message = e
+        except ModuleNotFoundError as e:
+            print(f"no module found in {module_path}")
+            error_message = e
+        except AttributeError as e:
+            print(f"no class '{main_classname}', found in module {module_path}")
+            error_message = e
+            
+        if not error_message:
+            return initiated_class
+        else:
+            print(error_message)
 
     def import_bot_module(self, module, name):
         module = __import__(module, fromlist=[name])
