9dec910c265617d76c20290daaf25107309c1017
jeppe <jeppemlv@gmail.com>
2022-01-11 20:53:33 +0100

    reddit scraper update

--- BotModules/GenericScrapers/RedditScrape.py
+++ BotModules/GenericScrapers/RedditScrape.py
@@ -1,71 +1,77 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from random import choice
+from iniparser import getConfigAsDict
 
 class RedditScrape:
-
-    
     def __init__(self, sub_reddit = "python", load_amount = 100,post_type = 'all'):
         self.sub_reddit = sub_reddit
         self.load_amount = load_amount
         self.post_dictionary = {}
         self.post_type = post_type
-        
-
-    #MemeBotButItsTrash
-    #JeppeChrisLasse123
-    def get_random_post(self):
-        if(len(self.post_dictionary) < 5):# and len(self.post_dictionary > 0)):
-            self.populate_list()
-      #  if(len(self.post_dictionary) == 0):
-       #    await self.populate_list()
-        key = choice(list(self.post_dictionary))
-        post = self.post_dictionary[key]
-        self.post_dictionary.pop(key)
-      #  print(self.post_dictionary.)
-       # print(len(self.post_dictionary))
-       # print(post)
-        return post
-
-    def populate_list(self):
-
+        if not self.setup_reddit_API_config():
+            raise Exception("Reddit Scrape missing config")
+            
+    def setup_reddit_API_config(self):
+        config = getConfigAsDict("REDDITSCRAPE")
+        if len(config) != 4:
+            return False
+            
         # note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
-        auth = HTTPBasicAuth('n_gaqCyYChHH3k1i6Ea2Tw', 'qh4_ESsCaGLSwYlK_n-omSjuiat9iw')
-
+        self.auth = HTTPBasicAuth(config["client_id"], config["secret_token"])
         # here we pass our login method (password), username, and password
-        data = {'grant_type': 'password',
-                'username': 'MemeBotButItsTrash',
-                'password': 'JeppeChrisLasse123'}
-
+        self.data = {'grant_type': 'password',
+                'username': config["username"],
+                'password': config["password"]}
+        
         # setup our header info, which gives reddit a brief description of our app
-        headers = {'User-Agent': 'MemeBot/0.0.1'}
+        self.headers = {'User-Agent': 'MemeBot/0.0.1'}
+        
+        return True
 
+
+    def get_reddit_api_token_header(self):
         # send our request for an OAuth token
         res = requests.post('https://www.reddit.com/api/v1/access_token',
-                            auth=auth, data=data, headers=headers)
+                            auth=self.auth, data=self.data, headers=self.headers)
         
         # convert response to JSON and pull access_token value
         TOKEN = res.json()['access_token']
 
         # add authorization to our headers dictionary
-        headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+        return {**self.headers, **{'Authorization': f"bearer {TOKEN}"}}
 
+    def get_random_post(self):
+        if(len(self.post_dictionary) < 5):# and len(self.post_dictionary > 0)):
+            self.populate_list()
+            
+        if not self.post_dictionary:
+            return {"title":"Link dictionary is empty :(", "url":"" }
+         
+        key = choice(list(self.post_dictionary))
+        post = self.post_dictionary[key]
+        self.post_dictionary.pop(key)
+        return post
+
+    def populate_list(self):
+        request_header_with_token = self.get_reddit_api_token_header()
         # while the token is valid (~2 hours) we just add headers=headers to our requests
-        requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
-
         res = requests.get("https://oauth.reddit.com/r/"+self.sub_reddit+"/hot",
-                   headers=headers, params = {'limit': str(self.load_amount)})
+                   headers=request_header_with_token, params = {'limit': str(self.load_amount)})
 
         #print(res.json())
         for idx, post in enumerate(res.json()['data']['children']):
                 
+            title = post['data']['title']
+            url = post['data']['url']
+                
             if (self.post_type == 'img' and url[-4:] in ('.jpg','.png','.gif','webm')):
-                post_info = {'title' : post['data']['title'],'url' : post['data']['url']}
+                post_info = {'title' :title,'url' : url}
                 self.post_dictionary[idx] = post_info
                 continue
             
             if (self.post_type == 'all'):
-                post_info = {'title' : post['data']['title'],'url' : post['data']['url']}
+                post_info = {'title' : title,'url' : url}
                 self.post_dictionary[idx] = post_info
                 continue
             
--- BotModules/reddit.py
+++ BotModules/reddit.py
@@ -1,7 +1,5 @@
-from random import choice
 from discord.ext import commands
-if __name__ != "__main__": 
-    from .GenericScrapers.RedditScrape import RedditScrape
+from BotModules.GenericScrapers.RedditScrape import RedditScrape
 
 class Reddit(commands.Cog):
     def __init__(self, config, bot: commands.Bot):
@@ -9,29 +7,28 @@ class Reddit(commands.Cog):
         self.config = config
         self.subreddit_collection = {}
        
-
     @commands.command()
     async def reddit(self, ctx,arg1: str, arg2: int = 100, arg3: str = 'all'):
         reddit_img = self.get_reddit(arg1,arg2,arg3)
         await ctx.send(reddit_img)
 
     def get_reddit(self,arg1,arg2 = 100,arg3 = 'all'):
-        #return choice(self.reddit_imgs)       
         if arg1 not in self.subreddit_collection:
-            self.subreddit_collection[arg1] = RedditScrape(sub_reddit=arg1,load_amount=arg2,post_type=arg3)
-        #self.scrape = RedditScrape(sub_reddit=arg1,load_amount=arg2,)
+            try:
+                self.subreddit_collection[arg1] = RedditScrape(sub_reddit=arg1,load_amount=arg2,post_type=arg3)
+            except Exception as e:
+                if arg1 in self.subreddit_collection:
+                    self.subreddit_collection.pop(arg1)
+                return "Could not retrieve subreddit posts"
         post = self.subreddit_collection[arg1].get_random_post()
         return f"{post['title']} \n {post['url']}"
-      #  print(self.scrape.get_random_post()[0])
-      #  return self.scrape.get_random_post()
 
 if __name__ == "__main__":
-    from GenericScrapers.RedditScrape import RedditScrape
     from configparser import ConfigParser
 
     config = ConfigParser()
     config.read("config.ini")
     bot = commands.Bot(command_prefix="!")
-    m = reddit(bot=bot, config=config["REDDIT"])
+    m = Reddit(bot=bot, config=config["REDDIT"])
     #m = Reddit(0,0)
     print(m.get_reddit("programmerhumor"))
