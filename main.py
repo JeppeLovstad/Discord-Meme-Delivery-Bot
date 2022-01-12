edb6366ad70a9dcf9c698492e34629ef5cb63272
Christoffer Changtrakul Jensen <christoffer@Christoffers-MacBook-Pro.local>
2022-01-12 16:02:45 +0100

    added more spaghetti to codebase

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from memebot import MemeBot
 from discord.ext import commands
+import discord
 import iniparser
 
     
 if __name__ == "__main__":
     config = iniparser.getConfigAsDict()
     if config:
+        intents = discord.Intents(messages=True, members=True)
         command_prefix = config["DISCORD"]["bot_command_prefix"]
-        bot = commands.Bot(command_prefix=command_prefix)
+        bot = commands.Bot(command_prefix=command_prefix, intents=intents)
         meme_bot = MemeBot(config,bot)
     else:
         print("Config is missing, quitting...")
