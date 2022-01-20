7bca7374fde71cc3d65cc08b58e01abdd365513e
Jeppe Løvstad <jml@solitwork.com>
2022-01-20 17:14:58 +0100

    moduleloader cont.

--- utils/moduleloader.py
+++ utils/moduleloader.py
@@ -13,45 +13,50 @@ def getModuleLoader(bot):
 
 
 class ModuleLoader:
-    dict_cogname_info = {}  # cog_name: [cog_info,enabled]
+    dict_cogname_info = {}  # cog_name: cog_info
     _bot: commands.Bot
 
     def __init__(self, bot: commands.Bot):
         self._bot = bot
         self.__update_cog_info_from_config()
 
-    def cog_status(self):
+    def is_cog_enabled(self, cog_name):
+        return self.dict_cogname_info[cog_name]["enabled"]
 
-        return
+    def load_cog(self, cog_name: str, force_reload=False):
+        if self.is_cog_enabled and force_reload:
+            self.unload_cog(cog_name)
+        else:
+            return "Cog is already loaded set force_reload = 1 to reload"
 
-    def load_cog(self, cog_name):
-        cog = self.__instantiate_cog_from_cog_info(cog_name)
+        cog = self.__get_instantiated_cog_from_cog_name(cog_name)
         if cog:
             self._bot.add_cog(cog)
 
     def unload_cog(self, cog_name):
         if cog_name in self.dict_cogname_info:
             self._bot.remove_cog(cog_name)
+            self.dict_cogname_info[cog_name]["enabled"] = False
             return f"Cog {cog_name} removed"
         else:
             return f"Could not remove {cog_name}, either it does not exist or something went wrong"
 
     def reload_all_cogs(self):
-        cog_info = self.__update_cog_info_from_config()
-        self.load_cogs(cog_info)
+        self.__update_cog_info_from_config()
+        self.load_cogs(self.dict_cogname_info)
 
     def reload_cog(self, cog_name):
         self.unload_cog(cog_name)
         self.load_cog(cog_name)
 
-    def load_cogs(self, cogs):
-        for cogInfo in cogs.values():
-            self.load_cog(cogInfo)
+    def load_cogs(self, cogs: dict):
+        for cog_name in list(cogs.keys()):
+            self.load_cog(cog_name)
 
     def sync_bot_with_config(self):
         pass
 
-    def __update_cog_info_from_config(self):
+    def __update_cog_info_from_config(self) -> None:
         config = iniparser.getConfigAsDict()
         for module, value in config["BOT_MODULES"].items():
             cog = {}
@@ -85,13 +90,12 @@ class ModuleLoader:
             else None
         )
 
-    def __instantiate_cog_from_cog_info(self, cog_name):
-        error_message = instantiated_cog = None
+    def __get_instantiated_cog_from_cog_name(self, cog_name: str):
         cog_info = self.__get_cog_info_from_name(cog_name)
+        return self.__get_instantiated_cog_from_cog_info(cog_info)
 
-        if cog_info is None:
-            print(f"Could not find cog with name {cog_name}")
-            return None
+    def __get_instantiated_cog_from_cog_info(self, cog_info):
+        error_message = instantiated_cog = None
 
         module, main_classname, module_path, module_config = (
             cog_info["module"],
@@ -100,23 +104,21 @@ class ModuleLoader:
             cog_info["module_config"],
         )
         try:
-
-            try:
-                # get class from folder
-                imported_class = self.__import_bot_module(module_path, main_classname)
-                # check if module specific config exists
-                # initate class
-                instantiated_cog = imported_class(bot=self._bot, config=module_config)
-                # update cog class name if another is specified
-                instantiated_cog.__cog_name__ = module.capitalize()
-            except Exception as e:
-                print(f"Could not instantiate {module} skipping..., \n Error: {e}")
-                error_message = e
+            # get class from folder
+            imported_class = self.__import_bot_module(module_path, main_classname)
+            # check if module specific config exists
+            # initate class
+            instantiated_cog = imported_class(bot=self._bot, config=module_config)
+            # update cog class name if another is specified
+            instantiated_cog.__cog_name__ = module.capitalize()
         except ModuleNotFoundError as e:
             print(f"no module found in {module_path}")
             error_message = e
         except AttributeError as e:
             print(f"no class '{main_classname}', found in module {module_path}")
+            error_message = e
+        except Exception as e:
+            print(f"Could not instantiate {module} skipping..., \n Error: {e}")
             error_message = e
 
         if not error_message:
@@ -138,5 +140,8 @@ if __name__ == "__main__":
     print(m.dict_cogname_info["Googler"], sep="\n")
     print(bot.cogs)
     m.load_cog("Googler")
-    print(bot.cogs)
+    print(bot.cogs, bot.commands)
+    print(m.dict_cogname_info["Googler"], sep="\n")
+    m.unload_cog("Googler")
+    print(bot.cogs, bot.commands)
     print(m.dict_cogname_info["Googler"], sep="\n")
