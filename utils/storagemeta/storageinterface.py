6fcadadc489b0680c46a1ba9259c5931f5891649
jeppe <jeppemlv@gmail.com>
2022-02-15 21:25:58 +0100

    interface work

--- utils/storagemeta/storageinterface.py
+++ utils/storagemeta/storageinterface.py
@@ -2,14 +2,29 @@ import abc
 
 
 class StorageMeta(abc.ABC):
+    
+    
     @abc.abstractmethod
-    def setup_connection() -> None:
+    def setup_storage_method(self, type:str= "", name: str = "", user: str = "", pasword: str = "", host:str = "", port: str = "") -> None:
         pass
-
-    @staticmethod
+    
     @abc.abstractmethod
-    def get_connection() -> None:
+    def store(self,key: str, value: object, module:str, server:str = "", channel:str = "", user:str = "", value_type: str = "str") -> bool:
         pass
-
-    def test(self):
-        return 1 
+    
+    @abc.abstractmethod
+    def retrieve(self, key: str, module:str, server:str = "", channel:str = "", user:str = "", value_type: str = "") -> object:
+        pass
+    
+    @abc.abstractmethod
+    def delete(self, key: str, module:str, server:str = "", channel:str = "", user:str = "") -> bool:
+        pass
+    
+    
+    @abc.abstractmethod
+    def store_message(self,key: str, value: object, module:str, server:str = "", channel:str = "", user:str = "", value_type: str = "str") -> bool:
+        pass
+    
+    @abc.abstractmethod
+    def retrieve_message(self, message_id: int, server:str = "", channel:str = "", user:str = "") -> str:
+        pass
