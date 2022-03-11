76b698c82e94ca8b5d4455fa47a86eade3b4333e
Jeppe Løvstad <jml@solitwork.com>
2022-03-11 11:06:08 +0100

    get last

--- BotModules/misc.py
+++ BotModules/misc.py
@@ -20,7 +20,7 @@ class Misc(commands.Cog):
         if text_to_sass is None or text_to_sass == "":
             text_to_sass = await ctx.channel.history(limit=2).flatten()
             # await ctx.send(text_to_sass[0])
-            sassed_text = self.sass_text(text_to_sass[0].content)
+            sassed_text = self.sass_text(text_to_sass[-1].content)
             await ctx.send(sassed_text)
         else:
             text_to_sass = " ".join(text_to_sass)
