98a3d7d70c35e38a7514539fafb9839b42fc7b8b
jeppe <jeppemlv@gmail.com>
2022-04-24 22:39:31 +0200

    interface

--- utils/storagemeta/storageinterface.py
+++ utils/storagemeta/storageinterface.py
@@ -1,42 +1,45 @@
-import abc
+from abc import ABC, abstractmethod
 from discord.ext import commands, tasks
 
 
-class StorageMeta(abc.ABC):
+class StorageMeta(ABC):
     
+    @abstractmethod
+    def __init__(self, type:str= "", name: str = "", user: str = "", password: str = "", host:str = "", port: str = ""):
+        self.__setup_storage_method__(type, name, user, password, host, port)
     
-    @abc.abstractmethod
-    def setup_storage_method(self, type:str= "", name: str = "", user: str = "", pasword: str = "", host:str = "", port: str = "") -> None:
+    @abstractmethod
+    def __setup_storage_method__(self, type:str= "", name: str = "", user: str = "", password: str = "", host:str = "", port: str = "") -> None:
         pass
     
-    @abc.abstractmethod
+    @abstractmethod
     def store(self,key: tuple[str, ...], value: object, module:str, server:str = "", channel:str = "", user:str = "", value_type: str = "str") -> bool:
         pass
     
-    @abc.abstractmethod
+    @abstractmethod
     def retrieve(self, key: tuple[str, ...], module:str, server:str = "", channel:str = "", user:str = "", value_type: str = "") -> object:
         pass
     
-    @abc.abstractmethod
+    @abstractmethod
     def delete(self, key: tuple[str, ...], module:str, server:str = "", channel:str = "", user:str = "") -> bool:
         pass
     
-    @abc.abstractmethod
+    @abstractmethod
     def store_message(self,key: tuple[str, ...], value: str, module:str, server:str = "", channel:str = "", user:str = "") -> bool:
         pass
     
-    @abc.abstractmethod
+    @abstractmethod
     def retrieve_message(self, message_id: int, server:str = "", channel:str = "", user:str = "", limit: int = 10) -> str:
         pass
     
-    @abc.abstractmethod
+    @abstractmethod
     def get_server(self, server_id:int): 
         pass
     
-    @abc.abstractmethod
+    @abstractmethod
     def get_channel(self, channel_id:int): 
         pass
     
-    @abc.abstractmethod
+    @abstractmethod
     def get_user(self, user_id:int): 
         pass
