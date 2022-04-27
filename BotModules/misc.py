729108e7df3e0d58a6907d36d47bbced507694e6
Jeppe Løvstad <jml@solitwork.dk>
2022-04-27 10:15:22 +0200

    pascal text

--- BotModules/misc.py
+++ BotModules/misc.py
@@ -15,9 +15,37 @@ class Misc(commands.Cog):
             ]
         )
 
+    def pascal_text(self, text_to_format: str) -> str:
+        return "".join([t.capitalize() for t in text_to_format.split(" ")])
+
+    def format_text(self, text_to_format: str, format: str = "sass") -> str:
+        if format == "sass":
+            return self.sass_text(text_to_format)
+        if format == "pascal":
+            return self.pascal_text(text_to_format)
+
+        return text_to_format
+
+    async def send_with_webhook(self, ctx, text):
+        webhook = await ctx.channel.create_webhook(name=ctx.author.name)
+        await webhook.send(
+            str(text),
+            username=ctx.author.nick,
+            avatar_url=ctx.author.avatar_url,
+        )
+
+        webhooks = await ctx.channel.webhooks()
+        for webhook in webhooks:
+            await webhook.delete()
+
     @commands.command(aliases=["s"])
     async def sass(
-        self, ctx, Messages_to_go_back: Optional[int] = 1, *, text_to_sass=""
+        self,
+        ctx,
+        Messages_to_go_back: Optional[int] = 1,
+        *,
+        text_to_sass="",
+        format: str = "sass"
     ):
 
         if Messages_to_go_back is None or Messages_to_go_back < 1:
@@ -27,20 +55,11 @@ class Misc(commands.Cog):
             text_to_sass = await ctx.channel.history(
                 limit=1 + Messages_to_go_back
             ).flatten()
-            sassed_text = self.sass_text(text_to_sass[-1].content)
+            sassed_text = self.format_text(text_to_sass[-1].content, format)
             await ctx.send(sassed_text)
         else:
-            sassed_text = self.sass_text(text_to_sass)
-            webhook = await ctx.channel.create_webhook(name=ctx.author.name)
-            await webhook.send(
-                str(sassed_text),
-                username=ctx.author.nick,
-                avatar_url=ctx.author.avatar_url,
-            )
-
-            webhooks = await ctx.channel.webhooks()
-            for webhook in webhooks:
-                await webhook.delete()
+            sassed_text = self.format_text(text_to_sass, format)
+            await self.send_with_webhook(ctx, sassed_text)
 
         await ctx.message.delete()
 
