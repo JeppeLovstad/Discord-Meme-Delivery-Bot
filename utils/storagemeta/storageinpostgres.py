02782654893e04b7851824e5a0de5ca9072f558b
Jeppe Løvstad <jml@solitwork.dk>
2022-04-27 14:05:12 +0200

    async connection

--- utils/storagemeta/storageinpostgres.py
+++ utils/storagemeta/storageinpostgres.py
@@ -1,94 +1,170 @@
 from abc import ABCMeta
 from storageinterface import StorageMeta
-import psycopg2 as pg2
+import psycopg as pg
 from sshtunnel import SSHTunnelForwarder
 import utils.iniparser as iniparser
 
+
 class StorageInPostgres(StorageMeta, metaclass=ABCMeta):
-    
+
     conn = None
     ssh_server = None
-    
+    postgres_args = None
+    ssh_tunnel_args = None
+
     def __init__(self):
-        postgres_args, ssh_tunnel_args = self.__get_config__()
-        if ssh_tunnel_args is not None and ssh_tunnel_args["sshtunnel"]:
-            self.setup_ssh_tunnel(postgres_args, ssh_tunnel_args)
-            
-        self.__setup_storage_method__(postgres_args)
+        self.postgres_args, self.ssh_tunnel_args = self.__get_config__()
+
+    async def __aenter__(self):
+        self.setup_ssh_tunnel(self.ssh_tunnel_args)
+        await self.__setup_storage_method__(self.postgres_args)
+        return self
 
     def __get_config__(self):
         config = iniparser.getConfigAsDict()
         if "POSTGRES" not in config:
-            raise Exception("Invalid POSTGRES configuration")
-        
+            raise Exception(
+                "Invalid POSTGRES configuration, must setup POSTGRES in config to use"
+            )
+
         postgres_args = config["POSTGRES"]
-        
-        if "SSH_TUNNEL" not in config:
-            #log: no ssh tunnel setup
-            print("no ssh tunnel setup")
         ssh_tunnel_args = config["SSH_TUNNEL"] if "SSH_TUNNEL" in config else None
-            
+
         return postgres_args, ssh_tunnel_args
 
-    def setup_ssh_tunnel(self, postgres_args, ssh_tunnel_args):
-        self.ssh_server = SSHTunnelForwarder(
-            (ssh_tunnel_args["ssh_host"], int(ssh_tunnel_args["ssh_port"])),
-            ssh_pkey=ssh_tunnel_args["ssh_cert_location"], 
-            ssh_private_key_password=ssh_tunnel_args["ssh_private_key_password"],
-            ssh_username=ssh_tunnel_args["ssh_user"],
-            remote_bind_address=(postgres_args["host"], int(postgres_args["port"])),
-            local_bind_address=(postgres_args["host"], int(postgres_args["port"]))
-            )
-        
-        ## Need to update port and host if using ssh tunnel
-        if self.ssh_server is not None:
-                self.ssh_server.start()
-                postgres_args["host"] = self.ssh_server.local_bind_host
-                postgres_args["port"] = self.ssh_server.local_bind_port
-    
-    def __setup_storage_method__(self, postgres_args) -> None:
-        
-        self.conn = pg2.connect(**postgres_args)
-    
-        cur = self.conn.cursor()
+    def setup_ssh_tunnel(self, ssh_tunnel_args):
+        if (
+            self.ssh_tunnel_args is not None
+            and self.postgres_args is not None
+            and self.ssh_tunnel_args["sshtunnel"]
+        ):
+            try:
+                self.ssh_server = SSHTunnelForwarder(
+                    (ssh_tunnel_args["ssh_host"], int(ssh_tunnel_args["ssh_port"])),
+                    ssh_pkey=ssh_tunnel_args["ssh_cert_location"],
+                    ssh_private_key_password=ssh_tunnel_args[
+                        "ssh_private_key_password"
+                    ],
+                    ssh_username=ssh_tunnel_args["ssh_user"],
+                    remote_bind_address=(
+                        self.postgres_args["host"],
+                        int(self.postgres_args["port"]),
+                    ),
+                    local_bind_address=(
+                        self.postgres_args["host"],
+                        int(self.postgres_args["port"]),
+                    ),
+                )
 
-        cur.execute('select * from public."TestTable"')
-        print(cur.fetchone())
-        
-        cur.close()
+                ## Need to update port and host if using ssh tunnel
+                if self.ssh_server is not None:
+                    self.ssh_server.start()
+                    self.postgres_args["host"] = self.ssh_server.local_bind_host
+                    self.postgres_args["port"] = self.ssh_server.local_bind_port
 
-    def store(self,key: tuple[str, ...], value: object, module:str, server:str = "", channel:str = "", user:str = "", value_type: str = "str") -> bool:
-        return True
-    
-    def retrieve(self, key: tuple[str, ...], module:str, server:str = "", channel:str = "", user:str = "", value_type: str = "") -> object:
-        pass
+            except:
+                print("Could not setup ssh_tunnel")
+        else:
+            print("ssh tunnel disabled or not setup")
 
-    def delete(self, key: tuple[str, ...], module:str, server:str = "", channel:str = "", user:str = "") -> bool:
-        return True
+    async def __setup_storage_method__(self, postgres_args) -> None:
+        self.conn = await pg.AsyncConnection.connect(**postgres_args)
 
-    def store_message(self,key: tuple[str, ...], value: str, module:str, server:str = "", channel:str = "", user:str = "") -> bool:
-        return True
-
-    def retrieve_message(self, message_id: int, server:str = "", channel:str = "", user:str = "", limit: int = 10) -> str:
-        return ''
-
-    def get_server(self, server_id:int): 
-        pass
-
-    def get_channel(self, channel_id:int): 
-        pass
-
-    def get_user(self, user_id:int): 
-        pass
-    
-    def close(self): 
+    ## test if connection is established
+    async def test(self):
         if self.conn is not None:
-            self.conn.close()
-        if self.ssh_server is not None:
-            self.ssh_server.close()
-    
+            cur = self.conn.cursor()
+            await cur.execute("select VERSION()")
+            async for record in cur:
+                print(record)
+            await cur.close()
+
+    def store(
+        self,
+        key: tuple[str, ...],
+        value: object,
+        module: str,
+        server: str = "",
+        channel: str = "",
+        user: str = "",
+        value_type: str = "str",
+    ) -> bool:
+        return True
+
+    def retrieve(
+        self,
+        key: tuple[str, ...],
+        module: str,
+        server: str = "",
+        channel: str = "",
+        user: str = "",
+        value_type: str = "",
+    ) -> object:
+        pass
+
+    def delete(
+        self,
+        key: tuple[str, ...],
+        module: str,
+        server: str = "",
+        channel: str = "",
+        user: str = "",
+    ) -> bool:
+        return True
+
+    def store_message(
+        self,
+        key: tuple[str, ...],
+        value: str,
+        module: str,
+        server: str = "",
+        channel: str = "",
+        user: str = "",
+    ) -> bool:
+        return True
+
+    def retrieve_message(
+        self,
+        message_id: int,
+        server: str = "",
+        channel: str = "",
+        user: str = "",
+        limit: int = 10,
+    ) -> str:
+        return ""
+
+    def get_server(self, server_id: int):
+        pass
+
+    def get_channel(self, channel_id: int):
+        pass
+
+    def get_user(self, user_id: int):
+        pass
+
+    async def __aexit__(self, *args):
+        await self.close()
+
+    async def close(self):
+        try:
+            if self.conn is not None:
+                await self.conn.close()
+            if self.ssh_server is not None:
+                self.ssh_server.close()
+        except:
+            print("Something went wrong while closing database connection")
+
+
+async def run():
+    async with StorageInPostgres() as dal:
+        await dal.test()
+
+
 if __name__ == "__main__":
-    
-    dal = StorageInPostgres()
-    dal.close()
-    
+    import asyncio
+    import sys
+
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    asyncio.run(run())
